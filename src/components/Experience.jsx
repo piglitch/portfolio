5750998364ebfafc3a1707f82351c5a9b2b13ce1
Avi <avibanerjee2000@gmail.com>
2023-06-24 20:44:30 +0530

    .

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -47,8 +47,8 @@ const Experience = () => {
   return (
     <>
     <motion.div variants={textVariant()}>
-      <p className={styles.sectionSubText}>Done so far</p>
-      <h2 className={styles.sectionHeadText}>Work exp</h2>
+      <p className={styles.sectionSubText}>Where I have worked before</p>
+      <h2 className={styles.sectionHeadText}>Work experience.</h2>
     </motion.div>
 
     <div className='mt-20 flex flex-col'></div>
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,15 +1,18 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { styles } from "../styles";
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
+    <div ><p className={styles.sectionHeadText}>Skills.</p>
+    <div className="flex flex-row flex-wrap justify-center mt-20 gap-10">
       {technologies.map((technology) => (
         <div className="w-28 h-28 key={technology.name}">
           <BallCanvas icon={technology.icon}/>
         </div>
       ))}
+    </div>
     </div>
   )
 }
